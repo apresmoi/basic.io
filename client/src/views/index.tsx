151d2b524de60a9aec95cd5fdfc359f45733d546
Juan Cruz Fortunatti <juancfortunatti@gmail.com>
2020-07-01 22:07:25 -0300

    Adding login_success event and player (self) component

--- client/src/views/index.tsx
+++ client/src/views/index.tsx
@@ -2,6 +2,18 @@ import React, { useState, useEffect, useRef } from 'react'
 import io from 'socket.io-client';
 const socketURL = `http://localhost:8081/`;
 
+interface Player {
+  id: string
+  x: number
+  y: number
+  dx: number
+  dy: number
+}
+
+interface WorldState {
+  self?: Player
+}
+
 const Container = (props) => {
   return <div style={{ display: 'flex', width: '100%', height: '100%' }}>
     <div style={{ width: 500, height: 500, background: 'white', margin: 'auto' }}>
@@ -12,14 +24,38 @@ const Container = (props) => {
   </div>
 }
 
-class World extends React.Component<{}, {}> {
+const Player = (props: Player) => {
+  const { x, y, id } = props
+  return <g transform={`translate(${x}, ${y})`}>
+    <circle r={10} />
+    <text textAnchor="middle" transform={`translate(0, 30)`}>{id}</text>
+  </g>
+}
+
+class World extends React.Component<{}, WorldState> {
   socket = io(socketURL, {
     path: `/ws`,
-    autoConnect: true,
+    autoConnect: false,
   });
 
+  constructor() {
+    super({})
+    this.state = {
+      self: null
+    }
+
+    this.socket.on('login_success', (payload) => {
+      console.log('login_success', payload)
+      this.setState({ self: payload })
+    })
+
+    this.socket.connect()
+  }
+
   render() {
+    const { self } = this.state
     return <Container>
+      {self && <Player {...self} />}
     </Container>
   }
 }
--- server/src/index.ts
+++ server/src/index.ts
@@ -7,11 +7,32 @@ const app = express();
 const http = new HttpServer(app);
 const io = socketio(http, { path: '/ws' });
 
+interface Player {
+  id: string
+  x: number
+  y: number
+  dx: number
+  dy: number
+}
+
+const players: { [id: string]: Player } = {}
+
 io.on('connect', (socket) => {
   console.log(`player ${socket.id} connected`)
 
+  players[socket.id] = {
+    id: socket.id,
+    x: Math.random() * 500,
+    y: Math.random() * 500,
+    dx: 0,
+    dy: 0,
+  }
+
+  socket.emit('login_success', players[socket.id])
+
   socket.on('disconnect', () => {
     console.log(`player ${socket.id} disconnected`)
+    delete players[socket.id]
   })
 });
 
